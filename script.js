c543e4c90481c8f558fb0cbb8d2c84631d159520
Estrbydesign <svma24zx@student.ju.se>
2025-02-21 17:43:07 +0100

    interactive

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttonElement = document.getElementById("dark-mode");
+
+let theme = "dark-mode" || "light-mode";
+localStorage.theme = theme;
+let newtheme = localStorage.theme;
+
+// if (theme === "dark-mode"){
+//     document.body.classList.add("dark-mode")
+// } else{
+//     document.body.classList.remove("dark-mode");
+// }
+
+
+
+
+buttonElement.addEventListener("click", function(event) {
+    document.body.classList.toggle("dark-mode");
+    // localStorage.theme = theme;
+
+});
+
+if (document.body.classList.contains("dark-mode")) {
+    localStorage.theme = "dark-mode";
+} else {
+    localStorage.theme = "light-mode";
+}
